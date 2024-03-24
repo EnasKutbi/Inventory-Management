566a322723607356a1dfd4f182262b99bc04c668
EnasKutbi <enaskutbi@gmail.com>
2024-03-25 01:42:30 +0300

    first step

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Item
+{
+    public string Name { get; }
+    private int Quantity { get; set; }
+    private DateTime CreatedDate { get; set; }
+
+    public Item(string name, int quantity, DateTime createdDate){
+        if (quantity < 0)
+        {
+            Console.WriteLine("quantity should be positive");
+        }
+        Name = name;
+        Quantity = quantity;
+        CreatedDate = createdDate;
+    }
+
+}
