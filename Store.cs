6487ee1911a741d432f4a765b44e6d8241965344
EnasKutbi <enaskutbi@gmail.com>
2024-03-28 14:03:30 +0300

    solve error

--- Store.cs
+++ Store.cs
@@ -11,26 +11,26 @@ public class Store
 
     public void AddItem(Item item)
     {
-        if (GetCurrentVolume() + item.Quantity > maximumCapacity)
+        if (GetCurrentVolume() + item.Quantity > _maximumCapacity)
         {
             Console.WriteLine("By adding this item you exceed the capacity of the store");
             return;
         }
 
-        bool isItemExist = items.Any(i => i.Name == item.Name);
+        bool isItemExist = _items.Any(i => i.Name == item.Name);
         if (isItemExist)
         {
             Console.WriteLine("This item already exists.");
             return;
         }
 
-        items.Add(item);
+        _items.Add(item);
         Console.WriteLine("Item added successfully");
     }
 
     public void PrintItems()
     {
-        foreach (var item in items)
+        foreach (var item in _items)
         {
             Console.WriteLine($"{item}");
         }
@@ -38,10 +38,10 @@ public class Store
 
     public void DeleteItem(string itemName)
     {
-        Item itemToBeDeleted = items.FirstOrDefault(i => i.Name == itemName);
+        Item itemToBeDeleted = _items.FirstOrDefault(i => i.Name == itemName);
         if (itemToBeDeleted != null)
         {
-            items.Remove(itemToBeDeleted);
+            _items.Remove(itemToBeDeleted);
             Console.WriteLine("Item deleted successfully");
         }
         else
@@ -52,12 +52,12 @@ public class Store
 
     public int GetCurrentVolume()
     {
-        return items.Sum(item => item.Quantity);
+        return _items.Sum(item => item.Quantity);
     }
 
     public Item FindItemByName(string itemName)
     {
-        Item findItem = items.FirstOrDefault(i => i.Name == itemName);
+        Item findItem = _items.FirstOrDefault(i => i.Name == itemName);
         if (findItem != null)
         {
             return findItem;
@@ -67,18 +67,18 @@ public class Store
 
     public List<Item> SortByNameAsc()
     {
-        return items.OrderBy(i => i.Name).ToList();
+        return _items.OrderBy(i => i.Name).ToList();
     }
 
     public IEnumerable<Item> SortByDate(SortOrder sortOrder)
     {
         if (sortOrder == SortOrder.ASC)
         {
-            return items.OrderBy(item => item.CreatedDate);
+            return _items.OrderBy(item => item.CreatedDate);
         }
         else
         {
-            return items.OrderByDescending(item => item.CreatedDate);
+            return _items.OrderByDescending(item => item.CreatedDate);
         }
     }
     public enum SortOrder
@@ -92,7 +92,7 @@ public class Store
         Dictionary<string, List<Item>> groupedItems = new Dictionary<string, List<Item>>();
         List<Item> old = new List<Item>();
         List<Item> newArrival = new List<Item>();
-        foreach (Item item in items)
+        foreach (Item item in _items)
         {
             if (item.CreatedDate >= DateTime.Now.AddMonths(-3))
             {
